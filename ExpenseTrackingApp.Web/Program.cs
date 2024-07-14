01d46605c60ce80bdda580de8b3cf7455aa8ec93
Aleksandar Zhelev <73350032+AleksZhelev@users.noreply.github.com>
2024-07-14 19:14:49 +0300

    Added Serilog to web project.

--- ExpenseTrackingApp.Web/Program.cs
+++ ExpenseTrackingApp.Web/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +13,15 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+using var log = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+Log.Logger = log;
+
+Log.Logger.Information("Logging");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- ExpenseTrackingApp.Web/obj/Debug/net8.0/ExpenseTrackingApp.Web.AssemblyInfo.cs
+++ ExpenseTrackingApp.Web/obj/Debug/net8.0/ExpenseTrackingApp.Web.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTrackingApp.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4fa5ac6882caad18f55770472cba29fce9f2dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTrackingApp.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTrackingApp.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
